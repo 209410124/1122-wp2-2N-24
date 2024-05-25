020c279354b41c73d6dcb82098dfc50f9bd8521b
unknown <209410124@gme.tku.edu.tw>
2024-05-25 20:42:53 +0800

    w14

---
+++ demo/w14-the-wild-oasis/final-6-final/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabaseUrl = "https://hsexmvemzjlqjqnfwwob.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzZXhtdmVtempscWpxbmZ3d29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYyMjM5MzMsImV4cCI6MjAwMTc5OTkzM30.J68ddL3WUrQkilObHdfnRkLeCdUHy8XP5ZjcF8cNISU";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
